3c24da8ea5e097cab2af58373a090c5fd9133c05
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2019-11-01 21:43:55 +0200

    init

--- HeartButton/ContentView.swift
+++ HeartButton/ContentView.swift
@@ -9,8 +9,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var colorChange = false
+    @State private var sizeChange = false
     var body: some View {
-        Text("Hello World")
+        Image(systemName: "heart.fill")
+            .font(.system(size: 200))
+            .foregroundColor(colorChange ? .yellow : .red)
+            .scaleEffect(sizeChange ? 1.5 : 1)
+            .animation(.default)
+            .onTapGesture {
+                self.colorChange.toggle()
+            }
+            .onLongPressGesture {
+                self.sizeChange.toggle()
+            }
     }
 }
 
